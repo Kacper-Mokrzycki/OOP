430f1ab0f99082cdd684d8d308b319708d0344f1
Kacper Mokrzycki <kacper.mokrzycki.contact@gmail.com>
2022-11-18 19:18:21 +0100

    corrected interface and class names and camelCase naming convention

--- task_4_v2/src/Main.java
+++ task_4_v2/src/Main.java
@@ -1,85 +1,85 @@
 import java.util.concurrent.TimeUnit;
 
-interface Vechicle_functions {
-    public void start_engine();
-    public void turn_off_engine();
-    public void drive_faster();
-    public void drive_slower();
+interface Vechicle {
+    public void startEngine();
+    public void turnOffEngine();
+    public void driveFaster();
+    public void driveSlower();
 }
 
 class Engine{
     private int volume;
-    private int horse_power;
+    private int horsePower;
     private int torque;
 
-    public Engine(int volume, int horse_power, int torque) {
+    public Engine(int volume, int horsePower, int torque) {
         this.volume = volume;
-        this.horse_power = horse_power;
+        this.horsePower = horsePower;
         this.torque = torque;
     }
 
 }
 
 
-class Vehicle implements Vechicle_functions{
-    public float gas_pedal_status = 0;
-    public boolean engine_status = false;
+class Car implements Vechicle{
+    public float gasPedalStatus = 0;
+    public boolean engineStatus = false;
     @Override
-    public void start_engine() {
-        this.engine_status = true;
-        this.gas_pedal_status = (float) 0.1;
+    public void startEngine() {
+        this.engineStatus = true;
+        this.gasPedalStatus = (float) 0.1;
     }
 
     @Override
-    public void turn_off_engine() {
-        this.engine_status = false;
-        this.gas_pedal_status = 0;
+    public void turnOffEngine() {
+        this.engineStatus = false;
+        this.gasPedalStatus = 0;
 
     }
 
     @Override
-    public void drive_faster() {
-        if (this.gas_pedal_status <=0.9) {
-            this.gas_pedal_status = this.gas_pedal_status + (float) 0.1;
+    public void driveFaster() {
+        if (this.gasPedalStatus <=0.9) {
+            this.gasPedalStatus = this.gasPedalStatus + (float) 0.1;
         }
     }
 
     @Override
-    public void drive_slower() {
-        if (this.gas_pedal_status >=0.1) {
-            this.gas_pedal_status = this.gas_pedal_status - (float) 0.1;
+    public void driveSlower() {
+        if (this.gasPedalStatus >=0.1) {
+            this.gasPedalStatus = this.gasPedalStatus - (float) 0.1;
         }
     }
 
     private Brand brand;
     private Engine engine;
-    private int max_speed;
+    private int maxSpeed;
 
-    public Vehicle Change_engine(Engine newEngine){ // Here is the Immutability implementation
-        return new Vehicle(brand,newEngine,max_speed);
+    public Car changeEngine(Engine newEngine){ // Here is the Immutability implementation
+        return new Car(brand,newEngine,maxSpeed);
     }
-    public Vehicle(Brand brand, Engine engine, int max_speed) {
+    public Car(Brand brand, Engine engine, int maxSpeed) {
         this.brand = brand;
         this.engine = engine;
-        this.max_speed = max_speed;
+        this.maxSpeed = maxSpeed;
     }
-    public String vehicle_speed(){
-        return String.format("%d", max_speed);
+    public String vechicleSpeed(){
+        return String.format("%d", maxSpeed);
     }
 }
 
 
 class Brand{
-    private String brand_name;
-    private int start_year;
-    private int number_of_employees;
+    private String brandName;
+    private int startYear;
+    private int numberOfEmployees;
     private String capitalization;
     private String debt;
 
-    public Brand(String brand_name, int start_year, int number_of_employees, String capitalization, String debt) {
-        this.brand_name = brand_name;
-        this.start_year = start_year;
-        this.number_of_employees = number_of_employees;
+    public Brand(String brandName, int startYear, int numberOfEmployees, String capitalization, String debt) {
+        this.brandName = brandName;
+        this.startYear = startYear;
+        this.numberOfEmployees = numberOfEmployees;
         this.capitalization = capitalization;
         this.debt = debt;
     }
@@ -90,7 +90,7 @@ public class Main {
     public static void main(String[] args) {
         Brand brand = new Brand("BMW", 1916, 118000, "53000000000", "15000000000");
         Engine engine = new Engine(2000,250,500);
-        Vehicle vehicle = new Vehicle(brand, engine, 250);
-        System.out.println(vehicle.vehicle_speed());
+        Car vehicle = new Car(brand, engine, 250);
+        System.out.println(vehicle.vechicleSpeed());
     }
 }
